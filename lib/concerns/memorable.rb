0cd5a01693e2fcc2f9589f6bd57cd8174ade1be4
MichaelAbe <michael.k.abe@gmail.com>
2020-11-25 15:59:06 -0500

    commit tree 62495a0a5e74dc5dc0c0dd079a34334f98c71db0

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,16 @@
+module Memorable
+
+
+
+     def reset_all
+        all.clear
+     end
+
+     def count
+        all.count
+    end
+
+    def find_by_name(name)
+        all.detect{|a| a.name == name}
+    end
+end
